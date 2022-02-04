f4b91edb7de1beaa4f0b8b56fde56a01e7b840d1
PranaliShingare <91608042+PranaliShingare@users.noreply.github.com>
2022-02-04 20:08:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TestFile from "./TestFile"
+
+function App() {
+  return (
+    <div className="App">
+      <p>React App</p>
+      <TestFile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TestFile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TestFile.css"
+
+function Test() {
+    return (
+      <div className="test">
+        <p>Test App</p>
+      </div>
+    );
+  }
+  
+  export default Test;
